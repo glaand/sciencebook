096f50236f55029c27374afd8c40c1aeaa6878a8
André Glatzl <andre.glatzl@fhgr.ch>
2024-03-19 17:50:57 +0100

    Added cypress

--- Makefile
+++ Makefile
@@ -13,8 +13,13 @@ down:
 logs:
 	docker compose logs -f
 
-test:
+component_test:
 	docker compose exec frontend yarn test
+
+e2e_test:
+	docker run -it -v ${PWD}/frontend:/e2e -w /e2e --network=host cypress/included:13.7.0
+
+test: component_test e2e_test
 
 shell:
 	docker compose exec frontend bash
---
+++ frontend/cypress/e2e/spec.cy.js
@@ -0,0 +1,19 @@
+describe('template spec', () => {
+    it('Open website', () => {
+        cy.visit('http://localhost:3000/')
+    })
+
+    /* ==== Test Created with Cypress Studio ==== */
+    it('Login and welcome message', function() {
+        /* ==== Generated with Cypress Studio ==== */
+        cy.visit('localhost:3000');
+        cy.get(':nth-child(1) > .grow').clear('f');
+        cy.get(':nth-child(1) > .grow').type('fhgr@fhgr.ch');
+        cy.get('.my-3 > .grow').clear();
+        cy.get('.my-3 > .grow').type('FHGR123?');
+        cy.get('.btn > span').click();
+        cy.get('.flex-col').click();
+        cy.get('.text-4xl').should('have.text', 'Welcome to Sciencebook👋');
+        /* ==== End Cypress Studio ==== */
+    });
+})
